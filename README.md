613b4aa872440ac6989464de0c2e3917df6c48de
richard-themedium <richard.jo@themedium.io>
2022-08-10 10:38:17 +0900

    first commit for testing

---
+++ README.md
@@ -0,0 +1,1 @@
+# fabric-kubernetes-richard
---
+++ preinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.8 1.5.3
+
+export PATH=$PATH:./fabric-samples/bin/
+
+# Generate MSP dir
+cryptogen generate --config=./crypto-config.yaml --output=./crypto-config
+
+# Generate genesis Tx
+export FABRIC_CFG_PATH=$PWD
+
+
+
+# Generate genesis block
+
+
