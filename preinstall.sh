68c67b8f38e23b34278d558752dd555ea96a9816
richard-themedium <richard.jo@themedium.io>
2022-08-12 19:32:16 +0900

    Tested running of orderer node container

--- preinstall.sh
+++ preinstall.sh
@@ -3,15 +3,18 @@
 curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.8 1.5.3
 
 export PATH=$PATH:./fabric-samples/bin/
-
 # Generate MSP dir
+
+echo "Generating MSP"
 cryptogen generate --config=./crypto-config.yaml --output=./crypto-config
+
+
 
 # Generate genesis Tx
 export FABRIC_CFG_PATH=$PWD
 
-
-
 # Generate genesis block
+
+
 
 
